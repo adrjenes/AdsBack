3f9fa893e19e52a2e016dc54634b604d783432e9
Adrian <adrjeness@gmail.com>
2022-09-25 14:52:23 +0200

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import 'express-async-errors';
 import cors from "cors";
+import {handleError} from "./utils/errors";
 
 const app = express();
 
@@ -8,7 +9,19 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+
+// Routes...
+
+app.get('/', async (req, res) => {
+    throw new Error('Daaamn!');
+})
+
+app.use(handleError);
+
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001');
 })
+
+
+
 
