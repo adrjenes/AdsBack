b8b9483433495bb3f99a68422844e62aa0d1923c
Adrian <adrjeness@gmail.com>
2022-10-02 15:55:27 +0200

    Added rate limiter.

--- index.ts
+++ index.ts
@@ -2,13 +2,18 @@ import express, { json } from "express";
 import 'express-async-errors';
 import cors from "cors";
 import {handleError} from "./utils/errors";
-
+import rateLimit from 'express-rate-limit';
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
 
 // Routes...
 
@@ -21,7 +26,3 @@ app.use(handleError);
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001');
 })
-
-
-
-
