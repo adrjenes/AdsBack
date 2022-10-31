9936dfe567c9751e4912ba049a66a5b992bc6c21
adrjenes <75531033+adrjenes@users.noreply.github.com>
2022-10-31 20:53:12 +0100

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -3,7 +3,7 @@ import {createPool} from "mysql2/promise";
 export const pool = createPool({
     host: 'localhost',
     user: 'root',
-    database: 'megak_ads',
+    database: 'ads',
     namedPlaceholders: true,
     decimalNumbers: true,
 
